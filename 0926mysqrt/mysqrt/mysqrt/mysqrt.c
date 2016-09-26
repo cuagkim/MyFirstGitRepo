c268244a682d8b0680e85e47e0f5a71f71403667
Dongeun Kim <cuagkim@gmail.com>
2016-09-26 16:53:27 +0900

    add convergence test

--- 0926mysqrt/mysqrt/mysqrt/mysqrt.c
+++ 0926mysqrt/mysqrt/mysqrt/mysqrt.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 	double x = 2.;
 	double s = 1.;
+	double tol = 1.0e-14;
+	int maxiter = 100;
+
 	int i;
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < maxiter; i++) {
 		printf("Before iteration %d, s = %20.15f\n", i, s);
+		double s0 = s;
 		s = 0.5*(s + x / s);
+		double delta_s = s - s0;
+		if (fabs(delta_s / x) < tol)
+			break;
 	}
 	printf("After iteration %d, s = %20.15f\n", i, s);
 	printf("mysqrt(%f) = %20.15f\n", x, s);
