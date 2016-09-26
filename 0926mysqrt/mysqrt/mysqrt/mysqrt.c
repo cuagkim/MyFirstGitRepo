08e37e211129a04cf6d1f8156f45386d2ee18262
Dongeun Kim <cuagkim@gmail.com>
2016-09-26 16:49:04 +0900

    print iteration

--- 0926mysqrt/mysqrt/mysqrt/mysqrt.c
+++ 0926mysqrt/mysqrt/mysqrt/mysqrt.c
@@ -3,11 +3,13 @@
 int main() {
 	double x = 2.;
 	double s = 1.;
-
-	for (int i = 0; i < 6; i++) {
+	int i;
+	for (i = 0; i < 6; i++) {
+		printf("Before iteration %d, s = %20.15f\n", i, s);
 		s = 0.5*(s + x / s);
 	}
-	printf("mysqrt(%f) = (%f)\n", x, s);
+	printf("After iteration %d, s = %20.15f\n", i, s);
+	printf("mysqrt(%f) = %20.15f\n", x, s);
 
 	return 0;
 
