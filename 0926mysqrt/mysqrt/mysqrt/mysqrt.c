51cd6c8793647f1e8cc9db8ecb30848c65bff08d
Dongeun Kim <cuagkim@gmail.com>
2016-10-12 19:39:30 +0900

    Copy last homework1

--- 0926mysqrt/mysqrt/mysqrt/mysqrt.c
+++ 0926mysqrt/mysqrt/mysqrt/mysqrt.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-
-void mysqrt(double x) {
-	double s = 1.;
-	double tol = 1.0e-14;
-	int maxiter = 100;
-
-	int i;
-	for (i = 0; i < maxiter; i++) {
-		printf("Before iteration %d, s = %20.15f\n", i, s);
-		double s0 = s;
-		s = 0.5*(s + x / s);
-		double delta_s = s - s0;
-		if (fabs(delta_s / x) < tol)
-			break;
-	}
-	printf("After iteration %d, s = %20.15f\n", i, s);
-	printf("mysqrt(%f) = %20.15f\n", x, s);
-
-}
+#include "mymath.h"
 
 int main() {
 	double x = 2.;
